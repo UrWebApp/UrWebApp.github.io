65b0ee06c1416da12beb69f6b9b25c59f601d7dc
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2023-01-06 21:31:52 +0800

    fix: theme layout

--- themes/webuild/source/js/main.js
+++ themes/webuild/source/js/main.js
@@ -1,11 +1,11 @@
 
 
-$("#categoriesBtn").click(()=>{
+$("#categoriesBtn").click(() => {
     $("#categories").slideToggle();
     $("#categoriesBtn i").toggleClass("fa-solid fa-square-caret-up").toggleClass("fa-solid fa-square-caret-down");
 })
 
-$("#tagsBtn").click(()=>{
+$("#tagsBtn").click(() => {
     $("#tags").slideToggle();
     $("#tagsBtn i").toggleClass("fa-solid fa-square-caret-up").toggleClass("fa-solid fa-square-caret-down");
 })
@@ -14,6 +14,10 @@ $(".input-group-input").addClass("form-control");
 $(".input-group-submit").addClass("btn btn-secondary");
 
 
-$('#barBtn').click(()=>{
+$('#barBtn').click(() => {
     $(".leftAside").slideToggle();
 })
+
+// 讓 Google Search 外開搜尋，並且搜尋後清空搜尋欄
+$(".input-group>button")[0].formTarget = '_blank';
+$(".input-group>button").click(() => { $(".input-group-input")[0].value = '' })
