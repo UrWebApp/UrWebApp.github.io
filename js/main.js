8ff6b1b1cdc5a767780b3a5c8012d8dbf1234ceb
L1uXO330B7 <L1uXO330B7@users.noreply.github.com>
2023-01-07 15:00:50 +0000

    deploy: 1831b41fef12f2bb6e5da4f867cec01e04597b1d

--- js/main.js
+++ js/main.js
@@ -21,3 +21,14 @@ $('#barBtn').click(() => {
 // 讓 Google Search 外開搜尋，並且搜尋後清空搜尋欄
 $(".input-group>button")[0].formTarget = '_blank';
 $(".input-group>button").click(() => { $(".input-group-input")[0].value = '' })
+
+// 頁角里程碑
+let DateNow = new Date();
+let DateDiff = function (sDate1, sDate2) {
+    var oDate1 = new Date(sDate1);
+    var oDate2 = new Date(sDate2);
+    var iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24); // 把相差的毫秒數轉換為天數
+    return iDays;
+};
+
+$("#milestone").html(`<span>Copyright © 2023-${DateNow.getFullYear()} UrWeb. All rights reserved.</span><br><span>This site is in orbit around internet ${DateDiff("2023/1/1",Date.now())} days.</span>`);
