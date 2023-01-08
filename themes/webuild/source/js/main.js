4d32684a1479cc0c4e9cd208c3fa4149bf24c418
johch3n611u <sjmjgd1165@gmail.com>
2023-01-08 12:12:37 +0800

    交給你了

--- themes/webuild/source/js/main.js
+++ themes/webuild/source/js/main.js
@@ -31,4 +31,10 @@ let DateDiff = function (sDate1, sDate2) {
     return iDays;
 };
 
-$("#milestone").html(`<span>Copyright © 2023-${DateNow.getFullYear()} UrWeb. All rights reserved.</span><br><span>This site is in orbit around internet ${DateDiff("2023/1/1",Date.now())} days.</span>`);
+$("#milestone").html(`
+<span>Copyright © 2023-${DateNow.getFullYear()} UrWeb. All rights reserved.</span><br>
+<span>This site is in orbit around internet ${DateDiff("2023/1/1",Date.now())} days.</span>
+<span class="material-symbols-outlined">sprint</span><br>
+<span class="material-symbols-outlined">transfer_within_a_station</span>
+<span class="waline-pageview-count" data-path="/" />
+people witnessed it all.`);
